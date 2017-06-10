ca40bd33fef0fbb5322c45b805a2c41fc956e700
Chisom Onu <chisomthaddeusonu@gmail.com>
2017-06-10 23:04:50 +0100

    Added Class Files

---
+++ GoBid.Utility/Cryptography.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GoBid.Utility
+{
+    public class Cryptography
+    {
+        public static string Encrypt(string encrypData)
+        {
+            string good = "";
+            try
+            {
+                for (int i = 0; i < encrypData.Length; i++)
+                {
+                    string charData = Convert.ToString(encrypData.Substring(i, 1));
+                    string conChar = char.ConvertFromUtf32(char.ConvertToUtf32(charData, 0) + 128);
+                    good = good + conChar;
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+
+            }
+            return good;
+
+
+        }
+
+        public static string Decrypt(string encrypData)
+        {
+            string good = "";
+            try
+            {
+                for (int i = 0; i < encrypData.Length; i++)
+                {
+                    string charData = Convert.ToString(encrypData.Substring(i, 1));
+                    string conChar = char.ConvertFromUtf32(char.ConvertToUtf32(charData, 0) - 128);
+                    good = good + conChar;
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+
+            }
+            return good;
+
+
+        }
+    }
+}
