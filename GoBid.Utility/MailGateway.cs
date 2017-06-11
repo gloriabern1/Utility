ba50da4a63c2b3774ff1eee90be3f2aae1b446ed
Chisom Onu <chisomthaddeusonu@gmail.com>
2017-06-11 07:54:20 +0100

    reformatting

--- GoBid.Utility/MailGateway.cs
+++ GoBid.Utility/MailGateway.cs
@@ -7,35 +7,38 @@ namespace GoBid.Utility
 {
     public class MailGateway
     {
-       public static string Sendmail(string smtpusername, string smtppassword, int smtpclientport,  string smtpclienthost, string Senderemail, string senderpassword, string Receivermail, string mailsubject, string mailmessage)
+        public static string Sendmail(string smtpusername, string smtppassword, int smtpclientport,
+            string smtpclienthost, string Senderemail, string senderpassword, string Receivermail, string mailsubject,
+            string mailmessage)
         {
             string Error;
-             string username = smtpusername; // e.g gloonyi@gmail.com, since i am using gmail smtp host
-             string password = smtppassword;
-            SmtpClient smtpclient = new SmtpClient();
-            System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage(); 
+            string username = smtpusername; // e.g gloonyi@gmail.com, since i am using gmail smtp host
+            string password = smtppassword;
+            var smtpclient = new SmtpClient();
+            var mail = new System.Net.Mail.MailMessage();
             MailAddress fromaddress = new MailAddress(Senderemail);
             //Set the email smtp host 
-            smtpclient.Host = smtpclienthost; // setting the smtp server it is sending the email from e.g gmail, yahoo, hotmail etc.
-           smtpclient.Port = smtpclientport;
+            smtpclient.Host = smtpclienthost;
+            // setting the smtp server it is sending the email from e.g gmail, yahoo, hotmail etc.
+            smtpclient.Port = smtpclientport;
             mail.From = fromaddress;
-           mail.To.Add(Receivermail);
-           mail.Subject = (mailsubject);
+            mail.To.Add(Receivermail);
+            mail.Subject = (mailsubject);
             mail.IsBodyHtml = true;
-           mail.Body = mailmessage;
+            mail.Body = mailmessage;
             smtpclient.DeliveryMethod = SmtpDeliveryMethod.Network;
             smtpclient.Credentials = new System.Net.NetworkCredential(username, password);
             try
             {
                 //Sending Email
                 smtpclient.Send(mail);
-               return Error="Email Has been sent successfully.";
+                return Error = "Email Has been sent successfully.";
             }
             catch (Exception ex)
             {
                 //Catch if any exception occurs
-               
-                return Error="Email Sending Fail:" + (ex.Message);
+
+                return Error = "Email Sending Fail:" + (ex.Message);
             }
         }
     }
