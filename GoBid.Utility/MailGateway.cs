a8bc4a866f92989401fa02ae2aae65e2c84a8db0
goddess12 <gloonyi12@gmail.com>
2017-06-12 08:01:43 +0100

    mail gateway update. check and tell me where to correct

--- GoBid.Utility/MailGateway.cs
+++ GoBid.Utility/MailGateway.cs
@@ -3,40 +3,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Net.Mail;
+
 namespace GoBid.Utility
+
 {
     public class MailGateway
     {
-       public static string Sendmail(string smtpusername, string smtppassword, int smtpclientport,  string smtpclienthost, string Senderemail, string senderpassword, string Receivermail, string mailsubject, string mailmessage)
+        public static SmtpClient smtpclient = new SmtpClient();
+        public static MailMessage mail = new MailMessage();
+        public static string Error;
+        public static string SendmailforRegisterconfirm(string Senderemail, string Receivermail, string mailsubject, string Username)
         {
-            string Error;
-             string username = smtpusername; // e.g gloonyi@gmail.com, since i am using gmail smtp host
-             string password = smtppassword;
-            SmtpClient smtpclient = new SmtpClient();
-            System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage(); 
-            MailAddress fromaddress = new MailAddress(Senderemail);
-            //Set the email smtp host 
-            smtpclient.Host = smtpclienthost; // setting the smtp server it is sending the email from e.g gmail, yahoo, hotmail etc.
-           smtpclient.Port = smtpclientport;
-            mail.From = fromaddress;
-           mail.To.Add(Receivermail);
-           mail.Subject = (mailsubject);
-            mail.IsBodyHtml = true;
-           mail.Body = mailmessage;
-            smtpclient.DeliveryMethod = SmtpDeliveryMethod.Network;
-            smtpclient.Credentials = new System.Net.NetworkCredential(username, password);
-            try
+         
+           try
             {
+                // initializing mail components
+                MailAddress fromaddress = new MailAddress(Senderemail);
+                mail.From = fromaddress;
+                mail.To.Add(Receivermail);
+                mail.Subject = (mailsubject);
+                mail.IsBodyHtml = true;
+                mail.Body = MailGateway.Emailhtmlcontent(Username); 
+                smtpclient.DeliveryMethod = SmtpDeliveryMethod.Network;
                 //Sending Email
+
                 smtpclient.Send(mail);
-               return Error="Email Has been sent successfully.";
+                return Error = "Email Has been sent successfully.";
             }
             catch (Exception ex)
             {
                 //Catch if any exception occurs
-               
-                return Error="Email Sending Fail:" + (ex.Message);
+
+                return Error = "Email Sending Fail:" + (ex.Message);
             }
+        }
+
+
+        public static string SendmailwithAttachment(string Senderemail, string Receivermail, string mailsubject, string Username, string attachmentpath)
+        {
+
+            try
+            {
+                // initializing mail components
+                MailAddress fromaddress = new MailAddress(Senderemail);
+                mail.From = fromaddress;
+                mail.To.Add(Receivermail);
+                mail.Subject = (mailsubject);
+                mail.IsBodyHtml = true;
+                mail.Body = MailGateway.Emailhtmlcontent(Username);
+                Attachment mailattachment = new Attachment(attachmentpath);
+                mail.Attachments.Add(mailattachment);
+                smtpclient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                //Sending Email
+
+                smtpclient.Send(mail);
+                return Error = "Email Has been sent successfully.";
+            }
+            catch (Exception ex)
+            {
+                //Catch if any exception occurs
+
+                return Error = "Email Sending Fail:" + (ex.Message);
+            }
+        }
+
+
+        public static string Emailhtmlcontent(string username)
+        {
+            StringBuilder SbBody = new StringBuilder();
+            SbBody.Append(" <html xmlns='http://www.w3.org/1999/xhtml'>");
+            SbBody.Append(" <head><title>Confirmation mail</title></head>");
+            SbBody.Append("< body >");
+            SbBody.Append("< img src = '//www.GoBid.com/images/Blue/Logo.png'/><br /><br />");
+            SbBody.Append("<div style = 'border-top:3px solid #22BCE5' > &nbsp;</div>");
+            SbBody.Append("<span style = 'font-family:Arial; font-size:10pt' >");
+            SbBody.Append("Hello < b >{username} click the button below to confirm your email and complete your registration <br/> <br/>");
+            SbBody.Append(" <a href='www.GoBid.com/Registration/Confirm.aspx' style='background-color:blue; font-size:14px'>Click Here</a>");
+            SbBody.Append("< /span >");
+            SbBody.Append("< /body >");
+            SbBody.Append("< /html >");
+            return SbBody.ToString();
         }
     }
 }
